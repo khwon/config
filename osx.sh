498da58db76b2f554bb4e1ce4a61027ca48dc85d
kanghee won <snowkh@gmail.com>
2014-08-21 12:29:31 +0900

    use brew for reattach-to-user-namespace

--- osx.sh
+++ osx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo "set-option -g default-command \"$DIR/reattach-to-user-namespace -l zsh\"" >> tmux.conf
+echo "set-option -g default-command \"reattach-to-user-namespace -l zsh\"" >> tmux.conf
