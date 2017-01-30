e6b843911171d1565f4a20c4a5de2a05daefe172
Kangheui Won <snowkh@gmail.com>
2017-01-30 16:38:03 +0900

    add thefuck config

--- setup.sh
+++ setup.sh
@@ -50,3 +50,6 @@ fi
 mkdir ~/.config
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+mkdir ~/.config/thefuck
+ln -s $DIR/thefuck/settings.py ~/.config/thefuck/settings.py
---
+++ thefuck/settings.py
@@ -0,0 +1,20 @@
+# The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+rules = ['git_pull', 'git_pull_uncommitted_changes', 'git_push_pull',
+        'git_push', 'sudo']
+
+# priority = {}
+# debug = False
+# require_confirmation = True
+# wait_command = 3
+# env = {'LANG': 'C', 'LC_ALL': 'C', 'GIT_TRACE': '1'}
+# rules = [<object object at 0x103292160>]
+# no_colors = False
+# exclude_rules = []
